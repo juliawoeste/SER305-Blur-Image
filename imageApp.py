efb072f4bb315f5457ea69ec6a604d214a4e762d
Humna Hanif <hhanif@quinnipiac.edu>
2023-05-06 19:02:29 -0400

    animation

--- imageApp.py
+++ imageApp.py
@@ -1,15 +1,66 @@
 from tkinter import *
 import tkinter as tk
 from PIL import ImageTk, Image, ImageFilter
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from moviepy.editor import *
 
 """ Animate the original image (based on the current blur value - which would NEED to be stored or accessed!)"""
 def animate():
-    print("ANIMATING!!!")
+    global original_image
+    # Create list of frames for gif
+    frames = []
+    
+    # Add frames going from original to blurred
+    for i in range(0, 101, 5):
+        blur_radius = i / 5
+        blurred_image = original_image.filter(ImageFilter.BoxBlur(radius=blur_radius))
+        frames.append(blurred_image)
+    
+    # Add frames going from blurred image to original image
+    for i in range(100, -1, -5):
+        blur_radius = i / 5
+        blurred_image = original_image.filter(ImageFilter.BoxBlur(radius=blur_radius))
+        frames.append(blurred_image)
+    
+    # Save frames as gif
+    frames[0].save('blur_animation.gif', format='GIF', append_images=frames[1:], save_all=True, duration=50, loop=0)
+    
+    # Open gif in new window
+    animation = tk.Toplevel(root)
+    animation.title("Blurring Animation")
+    animation.geometry("300x300")
+    
+    animation_label = tk.Label(animation)
+    animation_label.pack()
+    
+    gif = Image.open('blur_animation.gif')
+    animation_frames = []
+    try:
+        while True:
+            animation_frames.append(ImageTk.PhotoImage(gif))
+            gif.seek(gif.tell() + 1)
+    except EOFError:
+        pass
+    
+    def update_animation(frame_index):
+        animation_label.config(image = animation_frames[frame_index])
+        animation.after(50, update_animation, (frame_index + 1) % len(animation_frames))
+        
+    update_animation(0)
 
+def save_image():
+    file_type = [('Jpg files', '*.jpg')]
+    filename = tk.filedialog.asksaveasfilename(filetypes = file_type, defaultextension='.jpg')
+    if filename:
+        # Convert image to RGB mode (keep getting RGB error if you don't do this)
+        rgb_image = blurred_image.convert('RGB')
+        rgb_image.save(filename)
+
+    
 """ Invoked when slider is moved. Blur the image based on the new value and update the displayed image."""
 def slide_activate(v):
     global original_image
+    global blurred_image
     # TO DO: Check that an image has been selected - or else ignore (or report an error)
     # print("You have selected a blur value of " + v)
     v = int(v)/5 # Convert to integer (and scale it - way too blurry otherwise)
@@ -48,12 +99,17 @@ button = tk.Button(root, text = "Upload File", width = 20, command = upload_file
 image_label = tk.Label(root)  # This will be used to display the image
 original_imag = None # This variable will store the ORIGINAL image once loaded (keeping here so global scope)
 
-horizontal = Scale(root, from_= 0, to= 100, orient=HORIZONTAL, length= 200, command = slide_activate)
+global scaleVal
+horizontal = Scale(root, from_= 0, to= 100, resolution=10, orient=HORIZONTAL, length= 200, command = slide_activate)
 horizontal.pack(side = 'bottom')
-#horizontal.get()
+scaleVal = horizontal.get()
 
 button2 = Button(root, text = "Click to Animate the Image", bd = 5, command = animate)
 button2.pack(side = 'bottom')
+
+save_button = Button(root, text="Save Image", bd=5, command=save_image)
+save_button.pack(side='bottom')
+
     
 #my_label = Label(root, text = "hello")
 label.pack()
